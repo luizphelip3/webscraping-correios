2911a74f12ee4f6de31fea797401e941c9e6a79e
luizphelip3 <luiz_phelip3@hotmail.com>
2022-02-07 15:37:00 -0300

    Atualizacao de projeto.

--- src/index.ts
+++ src/index.ts
@@ -7,24 +7,25 @@ var cep = require('prompt');
 cep.start();
 
 // Recebe a propriedade "Cep" do usuário
-cep.get(['cep'], function (_err: any, result: { cep: number}) {
-  // Começa a printar os resultados para o usuário
+cep.get('cep', async function(_err: any, result: { cep: number}) {
+
+  // Executa o puppeteer
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Aciona a função "goto" para indicar o caminho que o navegador deve ir
+  await page.goto(`http://www.consultaenderecos.com.br/busca-cep/${result.cep}`)
+
+  // Retorna o resultado para o usuário
   console.log('O dado que você inseriu foi recebido: ' + result.cep + ' e o resultado da sua pesquisa está em endereço.png!');
-});
-    // Executa o puppeteer
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    JSON.stringify(cep)
-    
-    // Aciona a função "goto" para indicar o caminho que o navegador deve ir
-    await page.goto(`http://www.consultaenderecos.com.br/busca-cep/${cep}`);
-    // Print para debug
-    // console.log(`http://www.consultaenderecos.com.br/busca-cep/${cep}`)
-    
-    // Tira um print da página que o navegador acessou
-    await page.screenshot({ path: 'endereco.png' });
-    // Encerra o processo
-    await browser.close();
-  })();
+  
+  // Print para debug
+  //console.log(`http://www.consultaenderecos.com.br/busca-cep/${result.cep}`)
+  // Tira um print da página que o navegador acessou
+  await page.screenshot({ path: 'endereco.png' });
+  // Encerra o processo
+  await browser.close();  
+}); 
+})();
 
   
